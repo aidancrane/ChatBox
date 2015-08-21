4fe043498c5c1e70827338f5526e9f317f3bac2a
Aidan Crane <aidancrane78@gmail.com>
2015-08-21 14:24:19 +0100

    Made login post to root
    Made /login forward to / with posted cridentials to be processed.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
-import flask_sijax
+from flask import Flask, render_template, redirect, url_for, request
+#import flask_sijax
 app = Flask(__name__)
 
-@flask_sijax.route(app, "/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-     return render_template('index.html', name="Aidan")
+     if request.method == 'POST':
+          email = request.form['email']
+          password = request.form['password']
+          return ("Hello Kitty" + email + password)
+     else:
+          return render_template('index.html', name="Aidan")
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+
+     return render_template('login.html', name="Aidan")
+'''
+     error = None
+     if request.method == "POST":
+          if request.form["username"] != "admin" or request.form["password"] != "admin":
+               error = "Invalid cridentials"
+          else:
+               return redirect(url_for("boop"))
+          return render_template("/login.html", error=error)
+'''
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
