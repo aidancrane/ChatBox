e88130295718fc1f99050f33e3ba04b420fc0702
Aidan Crane <aidancrane78@gmail.com>
2015-09-07 16:05:49 +0100

    Made a better login page
    need to fix spacing on sign in button

--- main.py
+++ main.py
@@ -103,21 +103,8 @@ def admin_dashboard():
                admin.shutdown_server()#waits for the last request to be serverd before shutting down
                admin.log("Shutting Down...")
                admin.log("Server Terminated at " + admin.GetTime())
-               return render_template("/admin/dashboard.html", isShuttingDown="True")
-          if request.form['kill'] == 'Log Out':
-               admin.log("Logging out" + session['email'])
-               email = session['email']
-               del session['email']
-               del session['password']
-               return render_template("/admin/dashboard.html", isLoggingOut=email)
-
-
-
-
-
-
-
-
+               return render_template("/admin/shuttingdown.html")
+               
                #os._exit(1)
                # Do things
      return render_template("/admin/dashboard.html")
