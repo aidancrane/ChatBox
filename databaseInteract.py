c1e79440a0073fdc086997b98cd8a016a7326dd3
Aidan Crane <aidancrane78@gmail.com>
2015-09-10 23:31:09 +0100

    Created Basic Database Functions
    Made SQLite Basic functions work aswell as initial database setup, now
    I've Just gotta verify input and check users passwords, I also need to
    do IP's for Will, so I'll have to do that at some point.
    
    Also salts need to be generated.

--- databaseInteract.py
+++ databaseInteract.py
@@ -1,22 +1,42 @@
 from flask import request
 import sqlite3
+import hashlib
+
+def getLastUUID():
+    data = cursor.execute('SELECT * FROM Users WHERE UUID = (SELECT MAX(UUID)  FROM Users);')
+    uid = cursor.fetchall()
+    for row in uid:
+          data = row
+    return int(data[0])
 
 def buildDatabase():
-    cursor.execute('CREATE TABLE Users ( UUID NUMERIC, username TEXT, email BLOB, password BLOB, active NUMERIC, PRIMARY KEY(UUID));')
-    cursor.execute("INSERT INTO Users VALUES(1,'admin','admin@localhost','password',1)")
-    closedb()
+    cursor.execute('CREATE TABLE Users ( UUID NUMERIC, username TEXT, realname TEXT, email TEXT, password TEXT, active NUMERIC, PRIMARY KEY(UUID));')
+    detail = (hashPassword("password"), 1)
+    cursor.execute("INSERT INTO Users VALUES(1,'admin','admin','admin@localhost',? ,? )", detail)
+    connection.commit()
 
-def checkLogin(fill, password):
+def checkLogin(data, password):
+    #Check Emails
+    #Check username
+    #Check password matches
     pass
 
 def hashPassword(password):
-    pass
+    Salt = "fishes"
+    newHash = hashlib.sha1()
+    newHash.update(password.encode("utf-8") + Salt.encode("utf-8"))
+    return str(newHash.hexdigest())
 
 def getUser(username):
     pass
 
-def addUser(realName, userName, Email, password):
-    c.execute('CREATE TABLE Users (users aidan)')
+def addUser(realName, userName, Email, password, activeEmail):
+
+
+    detail = (getLastUUID() + 1, userName, realName, Email, hashPassword(password), activeEmail)
+    #print ("INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?)", detail)
+    cursor.execute("INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?)", detail)
+    connection.commit()
 
 def getPermission(username):
     pass
@@ -35,9 +55,13 @@ connection = sqlite3.connect("users.db")
 cursor = connection.cursor()
 connection.commit()
 
-def closedb():
+def closeCursor():
     cursor.close ()
     connection.close()
 
 
-buildDatabase()
+
+#buildDatabase()
+addUser("Aidan Crane", "aidan573", "aidancrane@gmail.com", "passvert", 1)
+#printData()
+closeCursor()
